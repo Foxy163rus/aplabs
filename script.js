97a7d411061ce684be9dca8a7fb7054a8d5a406b
Foxy163rus <62958994+Foxy163rus@users.noreply.github.com>
2020-10-31 13:24:49 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function calc(num1, operator, num2){
-	switch(operator){
-		case "+": return num1 + num2;
-		case "-": return num1 - num2;
-		case "*": return num1 * num2;
-		case "/": return num1 / num2;
-	}
-}
-console.log(calc(1,"+",2));
